655a475ba4e79dfee0a4012211cf27ee3bf9a0ef
Tyler Weisner <weisnert@cse.msu.edu>
2021-03-08 15:04:19 -0500

    Initial Commit

---
+++ Finstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Finstagram
+//
+//  Created by Tyler Weisner on 3/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
